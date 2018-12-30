fb3fa73a2a1ecf0883b1916e2893bc0fe8e59533
raskpask <35992491+raskpask@users.noreply.github.com>
2018-12-30 22:35:40 +0100

    Init

---
+++ fragmnts/photogallery.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE HTML >
+<html lang="en">
+
+<body>
+
+<div class="container">
+
+    <!-- Full-width images with number text -->
+    <div class="gallery">
+        <a href="../img/1.jpg" onclick="show()">
+            <img src="/img/1.jpg" width="300" height="200">
+        </a>
+
+        <a href="/img/2.jpg">
+            <img src="/img/2.jpg" width="300" height="200">
+        </a>
+
+        <a href="/img/3.jpg">
+            <img src="/img/3.jpg" width="300" height="200">
+        </a>
+
+        <a href="/img/4.jpg">
+            <img src="/img/4.jpg" width="300" height="200">
+        </a>
+
+        <a href="/img/5.jpg">
+            <img src="/img/5.jpg" width="300" height="200">
+        </a>
+
+        <a href="/img/6.jpg">
+            <img src="/img/6.jpg" width="300" height="200">
+        </a>
+        <a href="/img/7.jpg">
+            <img src="/img/7.jpg" width="300" height="200">
+        </a>
+
+        <a href="/img/8.jpg">
+            <img src="/img/8.jpg" width="300" height="200">
+        </a>
+
+        <a href="/img/9.jpg">
+            <img src="/img/9.jpg" width="300" height="200">
+        </a>
+    </div>
+
+
+
+</div>
+
+</body>
+<html>
