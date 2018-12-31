bc742bad7a4d05c3eb60ef733dbd9a4762d26cf9
Raskpask <molin.jakob@gmail.com>
2018-12-31 16:54:04 +0100

    menu done

--- JS/navbar.js
+++ JS/navbar.js
@@ -2,21 +2,25 @@ $(document).ready(function () {
 
 
     $("img#menuButton").click(function () {
-
+        menuDown();
+    });
+    function menuUp(){
         $("ul#navbar").empty();
-        $(  "<li>  <a href='/Index.php' id='navbutton'> <img src='/img/menu.png' id='menuButtonDown' >  Glyxnäs Multisport </a> </li> "+
-            "<li> <a href='/links/gallery.php' id='navbutton'> Bilder </a> </li>\n" +
+        $(  " <li>  <img src='/img/menu.png' id='menuButton'> <a href='/Index.php' id='navbutton'>  Glyxnäs Multisport </a> </li>").click(menuDown).appendTo($("ul#navbar"));
+    }
+    function menuDown(){
+        $("ul#navbar").empty();
+
+        $(  " <li> <img src='/img/menu.png' id='menuButtonDown' > <a href='/Index.php' id='navbutton'>  Glyxnäs Multisport </a> </li> " ).click(menuUp).appendTo($("ul#navbar"));
+        $(             " <li> <a href='/links/gallery.php' id='navbutton'> Bilder </a> </li>\n" +
             "    <li> <a href='/links/signup.php' id='navbutton'> Anmälan/Anmälda Lag </a> </li>\n" +
             "    <li> <a href='/links/results.php' id='navbutton'> Resultat </a> </li>\n" +
             "    <li> <a href='/links/info.php' id='navbutton'> Info </a> </li>").appendTo($("ul#navbar"));
-    });
-
+    }
     $("img#menuButtonDown").click(function () {
 
         $("ul#navbar").empty();
         //$(  " <li>  <img src=\"/img/menu.png\" id=\"menuButton\" > <a href='/Index.php' id='navbutton'>  Glyxnäs Multisport </a> </li>");
     });
-    function up() {
 
-    }
 });
